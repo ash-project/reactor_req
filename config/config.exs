5474bed028995a0ab0dfee96248399ffcba169d7
Zach Daniel <zach@zachdaniel.dev>
2025-06-10 16:12:40 -0400

    chore: update deps & config

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,7 @@ config :spark, formatter: [remove_parens?: true]
 
 config :git_ops,
   mix_project: Reactor.Req.MixProject,
+  github_handle_lookup?: true,
   changelog_file: "CHANGELOG.md",
   repository_url: "https://github.com/ash-project/reactor_req",
   manage_mix_version?: true,
